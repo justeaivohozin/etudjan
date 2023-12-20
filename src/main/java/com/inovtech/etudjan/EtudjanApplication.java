c6a391ba34fa61be3cbffaee36473cf6c63a3dc3
AIVOHOZIN <juste.aivohozin@cgi.com>
2023-12-20 19:03:22 +0100

    Commit files

---
+++ src/main/java/com/inovtech/etudjan/EtudjanApplication.java
@@ -0,0 +1,13 @@
+package com.inovtech.etudjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudjanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudjanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovtech/etudjan/EtudjanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovtech.etudjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudjanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
